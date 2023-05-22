956bb5d76e2a6213317b5984dd5ee34fa41541c1
unknown <bayuxbca@gmail.com>
2023-05-22 19:34:50 +0700

    seeder: camp table seeder

---
+++ database/seeders/CampTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Camp;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class CampTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $camps = [
+            [
+                'title' => 'Beginner Class',
+                'slug' => Str::slug('Beginner Class'),
+                'price' => 140,
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+            [
+                'title' => 'Intermediate Class',
+                'slug' => Str::slug('Intermediate Class'),
+                'price' => 280,
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time())
+            ]
+            ];
+
+        // first method ( not mandatory define created_at & updated_at )
+        // foreach ($camps as $key => $camp) {
+        //     Camp::create($camp);
+        // };
+
+        Camp::insert($camps);
+    }
+}
