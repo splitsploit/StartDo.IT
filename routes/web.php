57e867d5422c1632b2e4c552b4c8d52914652f0d
unknown <bayuxbca@gmail.com>
2023-05-23 21:47:40 +0700

    breeze: install & integrating

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,31 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'index');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-Route::get('login', function() {
-    return view('login');
-})->name('login');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('checkout', function() {
-    return view('checkout');
-})->name('checkout');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('success-checkout', function() {
-    return view('success_checkout');
-})->name('success-checkout');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
