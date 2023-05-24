d9d0a9b9f8e7edab14440bce5460ef66a84ee53d
unknown <bayuxbca@gmail.com>
2023-05-24 20:06:53 +0700

    socialite: integrating & fix HTTPS issue

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -33,5 +34,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+// socialite route
+Route::get('sign-in-google', [UserController::class, 'google'])->name('user.google.login');
+Route::get('/auth/google/callback', [UserController::class, 'handleProviderCallback'])->name('user.google.callback');
 
 require __DIR__.'/auth.php';
