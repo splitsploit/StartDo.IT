1746075ab4b39880163330a712f1b4f671d6a78e
unknown <bayuxbca@gmail.com>
2023-05-29 20:11:59 +0700

    request validation: finish at checkout

--- app/Http/Requests/User/Checkout/StoreRequest.php
+++ app/Http/Requests/User/Checkout/StoreRequest.php
@@ -23,8 +23,15 @@ class StoreRequest extends FormRequest
      */
     public function rules(): array
     {
+        $expiredValidation = date('Y-m', time());
+
         return [
-            'name' => 'required'
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email,'. Auth::id().',id',
+            'occupation' => 'required|string',
+            'card_number' => 'required|numeric|digits_between:8,16',
+            'expired' => 'required|date|date_format:Y-m|after_or_equal:'. $expiredValidation,
+            'cvc' => 'required|numeric|digits:3'
         ];
     }
 }
