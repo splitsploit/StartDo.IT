89bb0f7efcf205967ebeaa00eee7e8869d5a866a
unknown <bayuxbca@gmail.com>
2023-05-30 10:38:52 +0700

    mail: send email after user registered an account

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Mail\User\RegisterMail;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 use Laravel\Socialite\Facades\Socialite;
 
 class UserController extends Controller
@@ -27,7 +29,18 @@ class UserController extends Controller
             'email_verified_at' => date('Y-m-d H:i:s', time()),
         ];
 
-        $user = User::firstOrCreate(['email' => $data['email']], $data);
+        // $user = User::firstOrCreate(['email' => $data['email']], $data);
+
+        // check, email already register or not
+        $user = User::where('email', $data['email'])->first();
+        
+        // if email, not yet registered
+        if(!$user) {
+
+            $user = User::create($data);
+
+            Mail::to($user->email)->send(new RegisterMail($user));
+        }
 
         Auth::login($user, true);
 
