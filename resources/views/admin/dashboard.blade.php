5415893c7bfe2dff343f2752e40a4ae603ae2863
unknown <bayuxbca@gmail.com>
2023-06-02 19:21:10 +0700

    admin dashboard show all transaction & set to paid action

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,63 +1,59 @@
 @extends('layouts.app')
 
 @section('content')
-    
-    <section class="dashboard my-5">
-        <div class="container">
-            <div class="row text-left">
-                <div class=" col-lg-12 col-12 header-wrap mt-4">
-                    <p class="story">
-                        DASHBOARD
-                    </p>
-                    <h2 class="primary-header ">
-                        List Bootcamp Transaction
-                    </h2>
+    <div class="container">
+        <div class="row">
+            <div class="col-8 offset-2">
+                <div class="card">
+                    <div class="card-header">
+                        StartDo.IT List Transaction
+                    </div>
+                    <div class="card-body">
+                        @include('components.alert')
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>User</th>
+                                    <th>Camp</th>
+                                    <th>Price</th>
+                                    <th>Register Data</th>
+                                    <th>Paid Status</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($checkouts as $checkout)
+                                <tr>
+                                    <td>{{ $checkout->user->name }}</td>
+                                    <td>{{ $checkout->camp->title }}</td>
+                                    <td>{{ $checkout->camp->price }}k</td>
+                                    <td>{{ $checkout->created_at->format('M d Y') }}</td>
+                                    <td>
+                                        @if ($checkout->is_paid)
+                                            <span class="badge bg-success">Paid</span>
+                                        @else
+                                            <span class="badge bg-warning">Waiting</span>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        <form action="#">
+                                            @csrf
+                                            <button class="btn btn-primary btn-sm">
+                                                Set to Paid
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                                @empty
+                                    <tr>
+                                        <td colspan="3">No Data Transaction</td>
+                                    </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-            <div class="row my-5">
-                <table class="table">
-                    @include('components.alert')
-                    <tbody>
-                        @forelse ($checkouts as $checkout)
-                            <tr class="align-middle">
-                                <td width="18%">
-                                    <img src="{{ asset('assets/images/item_bootcamp.png') }}" height="120" alt="">
-                                </td>
-                                <td>
-                                    <p class="mb-2">
-                                        <strong>{{ $checkout->camp->title }}</strong>
-                                    </p>
-                                    <p>
-                                        {{ $checkout->created_at->format('M d Y') }}
-                                    </p>
-                                </td>
-                                <td>
-                                    <strong>${{ $checkout->camp->price }}</strong>
-                                </td>
-                                <td>
-                                    @if ($checkout->is_paid)
-                                        <strong class="text-success">Payment Success</strong>
-
-                                        @else
-                                        <strong>Waiting for Payment</strong>
-
-                                    @endif
-                                </td>
-                                <td>
-                                    <a href="https://wa.me/6282265157644/?text= Hai Admin, saya mau menanyakan kelas {{ $checkout->camp->title }}" class="btn btn-primary">
-                                        Contact Support
-                                    </a>
-                                </td>
-                            </tr>
-                        @empty
-                            <td colspan="5">
-                                <p>Nothing Data To Show</p>
-                            </td>
-                        @endforelse
-                    </tbody>
-                </table>
-            </div>
         </div>
-    </section>
-
+    </div>
 @endsection
